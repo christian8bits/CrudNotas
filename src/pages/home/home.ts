4702f17197e1239e345becd5704f04087072011e
christian8bits@gmail.com <christian8bits@gmail.com>
2019-11-11 17:40:57 -0300

    CRUD registro de lembretes, exemplo clicavel

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,4 +11,31 @@ export class HomePage {
 
   }
 
-}
+
+notes = [
+  {
+    id:'1',
+    date: '2016-02-01',
+    title: 'Firebase',
+    content: 'Que tal programar Serveless?'
+  },
+  {
+    id: '2',
+    date: '2016-01-01',
+    title: 'Ionic',
+    content: 'Aprenda o básico de ionic'
+  },
+  {
+    id: '3',
+    date: '2016-03-01',
+    title: 'Angular',
+    content: 'Importante para desenvolver com Ionic'
+  }
+  ]
+  onItemClick (note){
+  //consolse.log("item-clik", note)
+  this.navCtrl.push('DetailPage', {
+  noteParam: note
+  });
+  }
+} 
